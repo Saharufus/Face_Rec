0e6e78125e3a8a2531b6d656a452364ef51c5aea
SaharG <saharg92@gmail.com>
2022-01-22 18:33:43 +0200

    with numpy

--- preprocessing.py
+++ preprocessing.py
@@ -1,92 +1,46 @@
-import tensorflow as tf
+import numpy as np
+import os
+import config
+import cv2
+from sklearn.model_selection import train_test_split
 
 
-# -------------------------------------------------------------------------------------------------
-# helper functions
-# -------------------------------------------------------------------------------------------------
-def scaler():
-    """
-    scale the img to 105x105 pixels and pix vals [0, 1]
-    :param filepath: the path to *.jpg
-    :returns: scaled img"""
-    # load img
-    loaded_img = tf.io.read_file(filepath)
-    img = tf.io.decode_jpeg(loaded_img)
-    # resize
-    img = tf.image.resize(img, (105, 105))
-    # rescale
-    img = img / 255
-    return img
+def load_data(img_num=300):
+    """loads the data to three np.array (anchor, positive, negative)"""
+    # setting lists to fill
+    anchor = []
+    positive = []
+    negative = []
+
+    # getting img filepath
+    anc_file_list = os.listdir(config.ANC_PATH)
+    pos_file_list = os.listdir(config.POS_PATH)
+    neg_file_list = os.listdir(config.NEG_PATH)
+
+    for i in range(img_num):
+        # getting the image
+        anc_img = cv2.imread(os.path.join(config.ANC_PATH, anc_file_list[i]))
+        pos_img = cv2.imread(os.path.join(config.POS_PATH, pos_file_list[i]))
+        neg_img = cv2.imread(os.path.join(config.NEG_PATH, neg_file_list[i]))
+
+        # resize and rescale
+        anc_img = cv2.resize(anc_img, (105, 105)) / 255
+        pos_img = cv2.resize(pos_img, (105, 105)) / 255
+        neg_img = cv2.resize(neg_img, (105, 105)) / 255
+
+        # filling
+        anchor.append(anc_img)
+        positive.append(pos_img)
+        negative.append(neg_img)
+
+    # creating dataset
+    X = np.concatenate([np.array([anchor, positive]), np.array([anchor, negative])], axis=1)
+    y = np.concatenate([np.ones(img_num), np.zeros(img_num)], axis=0)
+
+    return X, y
 
 
-def label(data1, data2, is_twin=True):
-    """
-    label the img if same person or not
-    :param data1: anchor
-    :param data2: positive / negative
-    :param is_twin: True / False
-    :return: labeled dataset
-    """
-    # label 1 for same person
-    if is_twin:
-        return tf.data.Dataset.zip((data1, data2, tf.data.Dataset.from_tensor_slices(tf.ones(len(data1)))))
-    # label 0 for different person
-    elif not is_twin:
-        return tf.data.Dataset.zip((data1, data2, tf.data.Dataset.from_tensor_slices(tf.zeros(len(data1)))))
-
-
-def prepro_2_img(filepath1, filepath2, is_twin):
-    """this function is for mapping the preprocess on the dataset"""
-    return {'val image': scaler(filepath1), 'input image': scaler(filepath2)}, is_twin
-
-
-# -------------------------------------------------------------------------------------------------
-# create and preprocess for dataset
-# -------------------------------------------------------------------------------------------------
-def create_dataset(anchor, positive, negative):
-    """
-    creates a labeled dataset with positives and negatives
-    :param anchor: tf Dataset of anchor img
-    :param positive: tf Dataset of positive img
-    :param negative: tf Dataset of negative img
-    :return: tf Dataset of positive and negative against anchor with label
-    """
-    # label positive
-    positive_labeled = label({'val image': anchor, 'input image': positive}, True)
-    # label negative
-    negative_labeled = label({'val image': anchor, 'input image': positive}, False)
-    # concat, map and cache
-    data = positive_labeled.concatenate(negative_labeled)
-    data = data.map(prepro_2_img)
-    return data
-
-
-# -------------------------------------------------------------------------------------------------
-# train test split
-# -------------------------------------------------------------------------------------------------
-def split_train_test(data, train_size=0.8, batch_size=16, prefetch_size=8, shuffle=True):
-    """
-    splits the data into train_data and test_data
-    :param data: data to split
-    :param train_size: percentage of train out of data
-    :param batch_size: the batches size in the data
-    :param prefetch_size: data to prepare
-    :param shuffle: True or False if shuffle or not
-    :return: train_data, test_data
-    """
-    # shuffling:
-    if shuffle:
-        data = data.shuffle(buffer_size=len(data)*10)
-
-    # preparing the train data:
-    train = data.take(round(len(data)*train_size))
-    train = train.batch(batch_size)
-    train = train.prefetch(prefetch_size)
-
-    # preparing the test data:
-    test = data.skip(round(len(data)*train_size))
-    test = test.take(round(len(data)*(1-train_size)))
-    test = test.batch(batch_size)
-    test = test.prefetch(prefetch_size)
-
-    return train, test
+def train_test_img_split(imgs, labels, train_size=.8):
+    ind = range(len(labels))
+    train, test = train_test_split(ind, train_size=train_size)
+    return imgs[:, train, :, :, :], imgs[:, test, :, :, :], labels[train], labels[test]
